4ba0598835e19220a09f99a4ac61286223f032ee
SamStuckey <sam_stuckey@jinglepunks.com>
2018-02-21 12:07:51 -0500

    Build out form and results (no style)

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,5 @@
+class SongsController < ApplicationController
+  def search
+    raise 'we made it'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "search#index"
 
-  resources :songs
+  resources :songs do
+    collection do
+      get :search
+    end
+  end
 end
