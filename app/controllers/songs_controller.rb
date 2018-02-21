d4fe6e1810d52f7c8e238165fd85e79c7502ba3d
SamStuckey <sam_stuckey@jinglepunks.com>
2018-02-21 16:51:47 -0500

    Add query filtering

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,17 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
+    unless params[:song].blank?
+      @songs = @songs.where(title: params[:song])
+    end
+    unless params[:album].blank?
+      @songs = @songs.joins(:album).where(albums: { title: params[:album] })
+    end
+    unless params[:artist].blank?
+      @songs = @songs.joins(:artist).where(artists: { name: params[:artist] })
+    end
+
+    @songs.all
 
     respond_to do |format|
       format.js { render "songs/index" }
