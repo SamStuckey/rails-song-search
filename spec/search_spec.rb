72fc10ad197a83f16a5af64d83b43b02a68178f6
SamStuckey <sam_stuckey@jinglepunks.com>
2018-02-26 12:33:48 -0500

    Create test file

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,2 +1,9 @@
-require 'spec_helper'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rspec/rails"
 
+RSpec.describe "search", type: :request do
+  it 'works' do
+    expect(true).to be true
+  end
+end
