4273e3fa305bfa6c43b07483664542c7ad525f61
SamStuckey <sam_stuckey@jinglepunks.com>
2018-02-26 11:34:53 -0500

    Style table

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,16 +2,16 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
     unless params[:song].blank?
-      @songs = @songs.where(title: params[:song])
+      @songs = @songs.where("songs.title LIKE ?", "%#{params[:song]}%")
     end
     unless params[:album].blank?
-      @songs = @songs.joins(:album).where(albums: { title: params[:album] })
+      @songs = @songs.joins(:album).where("albums.title LIKE ?", "%#{params[:album]}")
     end
     unless params[:artist].blank?
-      @songs = @songs.joins(:artist).where(artists: { name: params[:artist] })
+      @songs = @songs.joins(:artist).where("artists.name LIKE ?", "%#{params[:artist]}%")
     end
 
-    @songs.all
+    @songs.all.limit(10)
 
     respond_to do |format|
       format.js { render "songs/index" }
